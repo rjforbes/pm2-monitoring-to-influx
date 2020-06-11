3da4a95e20cf95ae73c1c15f8923c53f4fdb3a5c
rj.forbes <rj.forbes@dplwireless.com>
2020-06-11 16:43:26 -0300

    1.1.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pm2-monitoring-to-influx
-Send basic pm2 monitoring data to influx db.
+####Send basic pm2 monitoring data to influx db.
+
+THis pm2 module send cpu and memory values for every service running in PM2 to influxdb.  This can then be easily displayed in Grafana for basic monitoring with historial data.
+
+To Install:
+```
+pm2 install pm2-monitoring-to-influx
+```
+
+Then set the interval to monitor in ms and the influxdb credentials. The influx database should already be created:
+```
+pm2 set pm2-monitoring-to-influx:interval 1000
+pm2 set pm2-monitoring-to-influx:host 'hostname'
+pm2 set pm2-monitoring-to-influx:port 8086
+pm2 set pm2-monitoring-to-influx:username 'user'
+pm2 set pm2-monitoring-to-influx:password 'pass'
+pm2 set pm2-monitoring-to-influx:database 'pm2-monitoring'
+```
+Then enable the service after configuration is set.
+```
+pm2 set pm2-monitoring-to-influx:enabled true
+```
---
+++ app.js
@@ -0,0 +1,116 @@
+const pm2 = require('pm2');
+const Influx = require('influx');
+const os = require('os');
+var pmx = require('pmx');
+
+pmx.initModule({
+
+  // Options related to the display style on Keymetrics
+  widget : {
+
+    // Logo displayed
+    logo : 'https://app.keymetrics.io/img/logo/keymetrics-300.png',
+
+    // Module colors
+    // 0 = main element
+    // 1 = secondary
+    // 2 = main border
+    // 3 = secondary border
+    theme            : ['#141A1F', '#222222', '#3ff', '#3ff'],
+
+    // Section to show / hide
+    el : {
+      probes  : true,
+      actions : true
+    },
+
+    // Main block to show / hide
+    block : {
+      actions : false,
+      issues  : true,
+      meta    : true,
+
+      // Custom metrics to put in BIG
+      main_probes : ['test-probe']
+    }
+
+  }
+
+}, function(err, config) {
+
+  
+pm2.connect(function(err) {
+    if (err) {
+      console.error("PM2 Connect Error" + err);
+    } else {
+        if(config.enabled){
+            console.log('Connecting to InfluxDB.');
+      
+            infclient = new Influx.InfluxDB({
+              host: config.host,
+              port: config.port,
+              username: config.username,
+              password: config.password,
+              database: config.database,
+              schema: [
+                {
+                  measurement: 'cpu',
+                  fields: {
+                    cpu_usage: Influx.FieldType.FLOAT
+                  },
+                  tags: [
+                    'host','service'
+                  ]
+                },
+                {
+                  measurement: 'memory',
+                  fields: {
+                    physical: Influx.FieldType.INTEGER
+                  },
+                  tags: [
+                    'host','service'
+                  ]
+                }
+              ]
+            });
+            console.log('Starting PM2 List Scan.');
+            pm2.list(errback);
+            setInterval(function() { pm2.list(errback);}, config.interval);
+        } else {
+            console.log('InfluxDB not enabled. Please configure and enable then restart the service.'); 
+        }
+    }
+  });
+  
+  function errback(err, resp){
+    if (err) {
+      console.error("PM2 List Error" + err);
+    } else {
+        var pointArray = [];
+        for (var index = 0; index < resp.length; index++) {
+          var element = resp[index];
+
+          console.log(element);
+          var cpu_usage = element.monit.cpu;
+          var physical = element.monit.memory;
+          pointArray.push({
+            measurement: 'cpu',
+            tags: { host: os.hostname() , service: element.name},
+            fields: { cpu_usage },
+          });
+          pointArray.push({
+              measurement: 'memory',
+              tags: { host: os.hostname() , service: element.name},
+              fields: { physical }
+           });
+
+        }
+        console.log(pointArray);
+        infclient.writePoints(pointArray).catch(err => {
+            console.error(`Error saving data to InfluxDB! ${err.stack}`);
+        });
+    }
+  }
+
+
+});
